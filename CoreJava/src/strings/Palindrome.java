84d3fad428da4acbbd6a5fb542c770620ccaef28
fyznitch <fyznitch@gmail.com>
2020-10-11 16:39:34 -0700

    Useful String stuff

---
+++ CoreJava/src/strings/Palindrome.java
@@ -0,0 +1,17 @@
+package strings;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		String word = "12345";
+		String reverse = "";
+		
+		for(int i = word.length() - 1; i >= 0;  i--) {
+			System.out.print(word.charAt(i));
+			
+			
+		}
+
+	}
+
+}
